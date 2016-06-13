5619917fc862675c863725905118e4157053d761
Nikolay Angelov <n.angelov@yahoo.com>
2016-06-13 17:46:41 +0200

    Fix td Height, Results Compare 1/2

--- script/app.js
+++ script/app.js
@@ -5,17 +5,56 @@ app.controller('betController',['$http','$sce', '$scope','$log',function($http, 
 
 var gl_games;
 var gl_teams;
-var check = this;
+
+var zalog = [   {nikolay: '2 : 0', ivan: '0 : 1', silviq: '?'},
+                {nikolay: '-', ivan: '-', silviq: '-'},
+                {nikolay: '-', ivan: '-', silviq: '-'},
+                {nikolay: '1 : 1', ivan: '2 : 1', silviq: '3 : 1'},
+                {nikolay: '-', ivan: '-', silviq: '-'},
+                {nikolay: '-', ivan: '-', silviq: '-'},
+                {nikolay: '1 : 0', ivan: '3 : 0', silviq: '2 : 0'},
+                {nikolay: '-', ivan: '-', silviq: '-'},
+                {nikolay: '-', ivan: '-', silviq: '-'},
+                {nikolay: '1 : 1', ivan: '2 : 2', silviq: '? : ?'},
+            ];
 
   $http.get('http://api.football-data.org/v1/soccerseasons/424/fixtures',
       { headers:{'X-Auth-Token': '2d9073763e924684a4162cea8d3817f4'}},{
       }).success(function (data) {
 
           var arr = data.fixtures;
-          var gl_games = data.fixtures;
+          gl_games = data.fixtures;
 
           $scope.results = arr;
           console.log(arr);
+
+          var arrayOne = [1,2,3,5];
+          var arrayTwo = [4,7,5,5];
+
+          for(var i =0; i<gl_games.length;i++){
+            if(gl_games[i].status.toString() === "FINISHED"){
+              for(var y =0; y<zalog.length;y++){
+
+                var home = gl_games[i].result.goalsHomeTeam.toString();
+                var away=  gl_games[i].result.goalsAwayTeam.toString();
+
+                var temp = home+":"+away;
+                var endString = temp.replace(/ /g, '');
+                console.log(endString);
+
+
+
+                if(endString === zalog[y].nikolay.toString().trim().replace(/ /g, '')){
+                  console.log("Match!");
+                }else{
+                }
+
+              }
+            }else{
+              console.log("Ne zavarshili " + i);
+            }
+
+          }
 
 
 
@@ -25,7 +64,7 @@ var check = this;
           { headers:{'X-Auth-Token': '2d9073763e924684a4162cea8d3817f4'}},{
           }).success(function (data) {
               var tmp = data.teams;
-              var gl_teams = data.teams;
+               gl_teams = data.teams;
               $scope.teams = tmp;
               console.log(tmp);
               console.log(gl_teams);
@@ -48,20 +87,13 @@ var check = this;
 
 
 
-  var zalog = [   {nikolay: '2 : 0', ivan: '0 : 1', silviq: '?'},
-                  {nikolay: '-', ivan: '-', silviq: '-'},
-                  {nikolay: '-', ivan: '-', silviq: '-'},
-                  {nikolay: '1 : 1', ivan: '2 : 1', silviq: '3 : 1'},
-                  {nikolay: '-', ivan: '-', silviq: '-'},
-                  {nikolay: '-', ivan: '-', silviq: '-'},
-                  {nikolay: '1 : 0', ivan: '3 : 0', silviq: '2 : 0'},
-                  {nikolay: '-', ivan: '-', silviq: '-'},
-                  {nikolay: '-', ivan: '-', silviq: '-'},
-                  {nikolay: '1 : 1', ivan: '2 : 2', silviq: '? : ?'},
-              ];
+
 
 
   $scope.zalog = zalog;
+
+
+
   $log.info($scope);
 
 }]);
